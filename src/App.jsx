ee55f778a320652b8b129956dfed8c095bd6bda0
IvanLeong03 <quantumoopsie@gmail.com>
2025-05-31 17:48:52 +0100

    deleted obsolete files, will make new ones to replace

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import './App.css'
-import Header from './Header'
-import Mainspace from './Mainspace'
-import Work from './Work'
-import About from './About'
-import Contact from './Contact'
+import { Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-    <div className="w-full flex justify-center bg-cream">
-      <div className="w-3/4 flex flex-col items-center bg-cream">
-      <Header />
-      <Mainspace />
-      <Work />
-      <About/>
-      <Contact />
-      </div>
+    <div className="relative w-full flex justify-center bg-cream">
+
+      
 
     </div>    
-    </>
+
   )
 }
 
